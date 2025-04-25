d8a058e1f629855584d432a4facecca56c0a13eb
T-Witmer <troykwitmer@gmail.com>
2025-04-24 20:25:57 -0400

    adding prediction

--- opennars/PredictorChannel.py
+++ opennars/PredictorChannel.py
@@ -14,12 +14,29 @@ nars_address = ("127.0.0.1", 54321)
 game_address = ("127.0.0.1", 12345)
 
 
-class PongChannel(SensorimotorChannel):
+class PredictorChannel(SensorimotorChannel):
 
-    def __init__(self, ID, num_slot, num_events, num_anticipations, num_operations, num_predictive_implications,
-                 num_reactions, N=1):
-        super().__init__(ID, num_slot, num_events, num_anticipations, num_operations, num_predictive_implications,
-                         num_reactions, N)
+    def __init__(
+        self,
+        ID,
+        num_slot,
+        num_events,
+        num_anticipations,
+        num_operations,
+        num_predictive_implications,
+        num_reactions,
+        N=1,
+    ):
+        super().__init__(
+            ID,
+            num_slot,
+            num_events,
+            num_anticipations,
+            num_operations,
+            num_predictive_implications,
+            num_reactions,
+            N,
+        )
         """
         Babbling is designed to be very simple. If the current channel cannot generate an operation based on the 
         reaction, then there is a certain probability that an operation will be generated through babbling. It is worth 
@@ -57,17 +74,17 @@ class PongChannel(SensorimotorChannel):
             return random.choice(list(self.operations.keys()))
 
 
-def execute_MLeft():
+def execute_Mup():
     """
     All channels need to register for its own operations. It is recommended to list them in the channel created.
     """
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.sendto("^left".encode(), game_address)
+    sock.sendto("^up".encode(), game_address)
 
 
-def execute_MRight():
+def execute_Mdown():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.sendto("^right".encode(), game_address)
+    sock.sendto("^down".encode(), game_address)
 
 
 def execute_Hold():
@@ -81,10 +98,10 @@ if __name__ == "__main__":
     "+1, +2" will not be in the Narsese.
     """
     r = Reasoner(100, 100)
-    pc = PongChannel("Pong", 2, 5, 50, 5, 50, 50, 1)
-    pc.register_operation("^left", execute_MLeft, ["^left", "left"])
-    pc.register_operation("^right", execute_MRight, ["^right", "right"])
-    pc.register_operation("^hold", execute_Hold, ["^hold", "mid"])
+    pc = PredictorChannel("Predictor", 2, 5, 50, 5, 50, 50, 1)
+    pc.register_operation("^up", execute_Mup, ["^up", "up"])
+    pc.register_operation("^down", execute_Mdown, ["^down", "down"])
+    pc.register_operation("^hold", execute_Hold, ["^hold", "hold"])
     for _ in range(1000):
         pc.channel_cycle(r.memory)
         pc.input_buffer.predictive_implications.show(lambda x: x.task.sentence)
